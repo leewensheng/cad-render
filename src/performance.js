ba5e4d1281c5cc4c1e3a1b2bf038fdeba1c9b846
liwensheng <527836355@qq.com>
2016-11-26 20:30:53 +0800

    zindex

--- src/performance.js
+++ src/performance.js
@@ -13,7 +13,7 @@ var Performance = React.createClass({
         var paper = cad.init({
                 el:el,
                 width:"100%",
-                height:window.innerHeight
+                height:window.innerHeight-10
             });
         paper.configLayer({
             stroke:"none",
@@ -22,9 +22,12 @@ var Performance = React.createClass({
         })
         paper.importBlock("chrome",100);
         paper.importSymbol('chrome2');
-        paper.use("chrome",0,0);
+        var p = paper.use("chrome",0,0);
         paper.use("chrome2",50,50,400,400).linkURL("http://www.baidu.com");
         paper.title("chrome")
+        paper.on("click",function(){
+            p.upperZIndex(1);
+        })
     }
 })
 module.exports = Performance;
