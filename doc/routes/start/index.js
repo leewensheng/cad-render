ecc44e784b40d5388640f00a3001a85b8b7fa2c4
liws <liws@mysoft.com.cn>
2016-12-22 19:05:28 +0800

    doc

---
+++ doc/routes/start/index.js
@@ -0,0 +1,7 @@
+import Start from '../../views/start/index.jsx'
+module.exports = {
+    path:'start',
+    component:Start,
+    childRoutes:[
+    ]
+}
---
+++ doc/views/start/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Link} from 'react-router'
+
+module.exports = React.createClass({
+    render(){
+        return <h1>quick-start</h1>
+    }
+})
